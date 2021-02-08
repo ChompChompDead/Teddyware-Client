a45fd5f7faa2d5a65f5b6556941121718c04cb25
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2021-02-08 12:23:56 -0600

    Add files via upload

---
+++ src/main/java/com/teddyhack/events/EventType.java
@@ -0,0 +1,6 @@
+package com.teddyhack.events;
+
+public enum EventType {
+    PRE,
+    POST,
+}
---
+++ src/main/java/com/teddyhack/events/listeners/EventUpdate.java
@@ -0,0 +1,8 @@
+package com.teddyhack.events.listeners;
+
+
+import com.teddyhack.events.Event;
+
+public class EventUpdate extends Event<EventUpdate> {
+
+}
