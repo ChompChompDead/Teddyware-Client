deee35bb75554787095af6c7238563fc0a824f24
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2021-02-10 16:56:55 -0600

    Update Sprint.java

--- src/main/java/com/teddyhack/module/movement/Sprint.java
+++ src/main/java/com/teddyhack/module/movement/Sprint.java
@@ -1,32 +1,34 @@
 package com.teddyhack.module.movement;
 
+import com.teddyhack.event.Event;
+import com.teddyhack.event.listeners.EventUpdate;
 import com.teddyhack.module.Category;
 import com.teddyhack.module.Module;
-import net.minecraft.client.Minecraft;
 import org.lwjgl.input.Keyboard;
 
 public class Sprint extends Module {
 
-    private Minecraft mc = Minecraft.getMinecraft();
-
     public Sprint() {
-        super("Sprint", "Automatically run without holding your run hotkey.", Category.MOVEMENT);
-        this.setKey(Keyboard.KEY_C);
+        super("Sprint", "Automatically run without holding your run hotkey.", Keyboard.KEY_C, Category.MOVEMENT);
     }
 
     @Override
     public void onEnable() {
-        super.onEnable();
 
-        if (mc.player.movementInput.moveForward >= 0) {
-            mc.player.setSprinting(true);
-        }
     }
 
     @Override
     public void onDisable() {
-        super.onDisable();
-
         mc.player.setSprinting(false);
+    }
+
+    @Override
+    public void onEvent(Event e) {
+        if(e instanceof EventUpdate) {
+            if(e.isPre()) {
+                if (mc.player.moveForward > 0 && !mc.player.isSneaking())
+                    mc.player.setSprinting(true);
+            }
+        }
     }
 }
