e510af0ff3988afc95e736b87f0455824a238a75
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2021-02-11 12:53:51 -0600

    Add files via upload

---
+++ src/main/java/com/teddyhack/module/client/DiscordRichPresence.java
@@ -0,0 +1,24 @@
+package com.teddyhack.module.client;
+
+import com.teddyhack.module.Category;
+import com.teddyhack.module.Module;
+import com.teddyhack.rpc.Discord;
+import org.lwjgl.input.Keyboard;
+
+public class DiscordRichPresence extends Module {
+
+    public DiscordRichPresence() {
+        super("DiscordRichPresence", "dee cord rich presence :)", Keyboard.KEY_NONE, Category.CLIENT);
+        this.toggled = true;
+    }
+
+    @Override
+    public void onEnable() {
+        Discord.startRPC();
+    }
+
+    @Override
+    public void onDisable() {
+        Discord.stopRPC();
+    }
+}
