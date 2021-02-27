b678b34058b799446b388d3c904b11667a646dbc
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2021-02-26 20:45:53 -0600

    Update ModuleManager.java

--- src/main/java/com/teddyhack/module/ModuleManager.java
+++ src/main/java/com/teddyhack/module/ModuleManager.java
@@ -1,118 +1,92 @@
-package com.teddyhack.module.render;
+package com.teddyhack.module;
 
-import com.teddyhack.event.Event;
+import com.teddyhack.Client;
 import com.teddyhack.event.listeners.EventKey;
-import com.teddyhack.event.listeners.EventRenderGUI;
-import com.teddyhack.module.Category;
-import com.teddyhack.module.Module;
-import com.teddyhack.module.ModuleManager;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.Gui;
+import com.teddyhack.module.client.ChatSuffix;
+import com.teddyhack.module.client.FancyChatMessages;
+import com.teddyhack.module.exploits.ServerBackdoor;
+import com.teddyhack.module.movement.Fly;
+import com.teddyhack.module.movement.Sprint;
+import com.teddyhack.module.movement.Step;
+import com.teddyhack.module.player.NoFall;
+import com.teddyhack.module.render.FullBright;
+import com.teddyhack.module.render.TabGUI;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.lwjgl.input.Keyboard;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TabGUI extends Module {
+public class ModuleManager {
 
-    public int currentTab;
-    public boolean expanded;
+    public static ArrayList<Module> modules;
 
-    public TabGUI() {
-        super("TabGUI", "clickgui but worse", Keyboard.KEY_NONE, Category.RENDER);
-        this.toggled = true;
+    public ModuleManager() {
+        modules = new ArrayList<>();
+
+        // player
+        modules.add(new Sprint());
+        modules.add(new Fly());
+        modules.add(new Step());
+
+        // Render
+        modules.add(new FullBright());
+        modules.add(new TabGUI());
+
+        // Client
+        modules.add(new ChatSuffix());
+        modules.add(new FancyChatMessages());
+
+        // Player
+        modules.add(new NoFall());
+
+        // Exploits
+        modules.add(new ServerBackdoor());
     }
 
-    @Override
-    public void onEvent(Event e) {
-        if (e instanceof EventRenderGUI) {
-            // Main tab
-            FontRenderer fr = mc.fontRenderer;
-
-            Gui.drawRect(5, 30, 75, 30 + Category.values().length * 16 + 2, 0x90000000);
-            Gui.drawRect(7, 33 + currentTab * 16, 7 + 61, 33 + currentTab * 16 + 12, 0xff783F04);
-
-            int count = 0;
-            for (Category c : Category.values()) {
-                fr.drawStringWithShadow(c.name, 11, 35 + count * 16, -1);
-
-                count++;
-            }
-
-            if (expanded) {
-                // Multiple tabs
-                Category category = Category.values()[currentTab];
-                List<Module> modules = ModuleManager.getModulesByCategory(category);
-
-                if (modules.size() == 0)
-                    return;
-
-                //background
-                Gui.drawRect(75, (int) 30.5, 75 + 68, 30 + modules.size() * 16 + (int) 1.5, 0x90000000);
-                //outline
-                Gui.drawRect(75, 33 + category.moduleIndex * 16, 10 + 61 + 68, 33 + category.moduleIndex * 16 + 12, 0xff783F04);
-
-                count = 0;
-                for (Module m : modules) {
-                    // names
-                    fr.drawStringWithShadow(m.name, 79, 35 + count * 16, -1);
-
-                    count++;
-                }
+    public static Module getModule(String name) {
+        for (Module m : modules) {
+            if (m.getName().equalsIgnoreCase(name)) {
+                return m;
             }
         }
-        // make tabgui work lol
-        if (e instanceof EventKey) {
-            int code = ((EventKey) e).code;
-            Category category = Category.values()[currentTab];
-            List<Module> modules = ModuleManager.getModulesByCategory(category);
-
-            if (code == Keyboard.KEY_UP) {
-                if (expanded) {
-                    if (category.moduleIndex <= 0) {
-                        category.moduleIndex = modules.size() - 1;
-                    } else {
-                        category.moduleIndex--;
-                    }
-                } else {
-                    if (currentTab <= 0) {
-                        currentTab = Category.values().length - 1;
-                    } else {
-                        currentTab--;
-                    }
-                }
-            }
-
-            if (code == Keyboard.KEY_DOWN) {
-                if (expanded) {
-                    if (category.moduleIndex >= modules.size() - 1) {
-                        category.moduleIndex = 0;
-                    } else {
-                        category.moduleIndex++;
-                    }
-                } else {
-                    if (currentTab >= Category.values().length - 1) {
-                        currentTab = 0;
-                    } else {
-                        currentTab++;
-                    }
-                }
-            }
-            if (code == Keyboard.KEY_RIGHT) {
-                if (expanded && modules.size() > 0) {
-                    Module module = modules.get(category.moduleIndex);
-                    if (!module.name.equals("TabGUI")) {
-                        module.toggle();
-                    }
-                } else if (modules.size() == 0) {
-                    expanded = false;
-                } else if (!expanded) {
-                    expanded = true;
-                }
-            }
-            if (code == Keyboard.KEY_LEFT) {
-                expanded = false;
-            }
-        }
+        return null;
     }
 
+    public ArrayList<Module> getModuleList() {
+        return modules;
+    }
+
+    public static List<Module> getModulesByCategory(Category c) {
+        List<Module> modules = new ArrayList<Module>();
+
+        for (Module m : ModuleManager.modules) {
+            if (m.getCategory() == c)
+                modules.add(m);
+        }
+        return modules;
+    }
+
+    @SubscribeEvent
+    public void key(InputEvent.KeyInputEvent key) {
+        if(Minecraft.getMinecraft().world == null || Minecraft.getMinecraft().player == null)
+            return;
+        try {
+            if(Keyboard.isCreated()) {
+                if(Keyboard.getEventKeyState()) {
+                    int keyCode = Keyboard.getEventKey();
+                    if(keyCode <= 0)
+                        return;
+                    Client.onEvent(new EventKey(keyCode));
+                    for(Module m : ModuleManager.modules) {
+                        if(m.getKey() == keyCode && keyCode > 0) {
+                            m.toggle();
+                        }
+                    }
+                }
+            }
+        } catch (Exception q) { q.printStackTrace(); }
+    }
 }
