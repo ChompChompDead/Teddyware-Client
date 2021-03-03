b23a48d7e5b9cf2c94309b7b66867088c022cf12
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2021-03-02 20:40:15 -0600

    Update MixinEntityPlayerSP.java

--- src/main/java/com/teddyhack/mixin/mixins/MixinEntityPlayerSP.java
+++ src/main/java/com/teddyhack/mixin/mixins/MixinEntityPlayerSP.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityPlayerSP.class)
 public class MixinEntityPlayerSP {
     @Inject(method = "onUpdateWalkingPlayer", at = @At("HEAD"), cancellable = true)
-    public void onUpdateWalkingPlayer(CallbackInfo ci) {
+    public void onUpdateWalkingPlayer(CallbackInfo info) {
         EventUpdate e = new EventUpdate();
         e.setType(EventType.PRE);
         Client.onEvent(e);
