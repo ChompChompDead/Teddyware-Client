6370d271a754c6d120ce716865e11b18f655ecb7
ChompChompDead <64820646+ChompChompDead@users.noreply.github.com>
2021-03-04 12:55:16 -0600

    Add files via upload

---
+++ src/main/java/com/teddyhack/Discord.java
@@ -0,0 +1,30 @@
+package com.teddyhack;
+
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRichPresence;
+import club.minnced.discord.rpc.DiscordRPC;
+
+public class Discord {
+    private static String discordID = "809223921575395370";
+    private static DiscordRichPresence discordRichPresence = new DiscordRichPresence();
+    private static DiscordRPC discordRPC = DiscordRPC.INSTANCE;
+
+    public static void startRPC() {
+        DiscordEventHandlers eventHandlers = new DiscordEventHandlers();
+        eventHandlers.disconnected = ((var1, var2) -> System.out.println("Discord RPC disconnected, var1: " + var1 + ", var2: " + var2));
+
+        discordRPC.Discord_Initialize(discordID, eventHandlers, true, null);
+
+        discordRichPresence.startTimestamp = System.currentTimeMillis() / 1000L;
+        discordRichPresence.details = "Version " + Client.VERSION;
+        discordRichPresence.largeImageKey = "teddyhacklogo";
+        discordRichPresence.largeImageText = ":)";
+        discordRichPresence.state = null;
+        discordRPC.Discord_UpdatePresence(discordRichPresence);
+    }
+
+    public static void stopRPC() {
+        discordRPC.Discord_Shutdown();
+        discordRPC.Discord_ClearPresence();
+    }
+}
