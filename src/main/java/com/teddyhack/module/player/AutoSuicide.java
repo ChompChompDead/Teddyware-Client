742f96b5780c3f2a5a2acd7da4ae2d3b03e01ed0
BuildTools <unconfigured@null.spigotmc.org>
2021-03-06 16:07:49 -0600

    revamp autosuicide

---
+++ src/main/java/com/teddyhack/module/player/AutoSuicide.java
@@ -0,0 +1,26 @@
+package com.teddyhack.module.player;
+
+import com.teddyhack.module.Category;
+import com.teddyhack.module.Module;
+import com.teddyhack.module.ModuleManager;
+import com.teddyhack.util.ChatUtil;
+import org.lwjgl.input.Keyboard;
+
+public class AutoSuicide extends Module {
+
+    public AutoSuicide() {
+        super("AutoSuicide", "Automatically kills yourself.", Keyboard.KEY_NONE, Category.Player);
+    }
+
+    @Override
+    public void onEnable() {
+        if (mc.player != null) {
+            if (ModuleManager.getModule("ChatSuffix").toggled) {
+                ChatUtil.type("You need to disable chatsuffix for AutoSuicide to work!");
+            } else {
+                mc.player.sendChatMessage("/kill");
+                toggle();
+            }
+        }
+    }
+}
