86486bd04f169d0aec4f7c83de1802a1c96ad74b
frostbyte <78167549+Officer32k@users.noreply.github.com>
2021-06-16 00:13:06 +0300

    Created welcomer

---
+++ src/main/java/com/chompchompdead/teddyware/client/module/hud/Welcomer.java
@@ -0,0 +1,12 @@
+package com.chompchompdead.teddyware.client.module.hud;
+
+import com.chompchompdead.module.Module;
+/**
+*@author frostbyte
+**/
+
+public class Welcomer extends Module {
+  public Welcomer() {
+    
+  }
+}
